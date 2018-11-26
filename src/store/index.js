a47c01db2f969674f759ab39a6d08efe94f218b9
dreamthen <dreamthen.00@gmail.com>
2018-11-27 00:12:50 +0800

    @尹文楷 build:修改redux store配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,22 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import rootReducer from '../reducers'
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
+// import {persistState} from 'redux-devtools';
+import rootReducer from '../reducers';
 
-const middlewares = [
-  thunkMiddleware
-]
+let middlewares = [];
 
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(require('redux-logger').createLogger())
+  middlewares = [...middlewares, thunkMiddleware, loggerMiddleware];
+} else {
+  middlewares = [...middlewares, thunkMiddleware];
 }
 
-export default function configStore () {
-  const store = createStore(rootReducer, applyMiddleware(...middlewares))
-  return store
-}
+const store_compose = compose(
+  applyMiddleware(...middlewares)
+  // persistState(window.location.href.match(/[?&]debug_sessions=([^&]+)\b/))
+)(createStore);
+
+const store = store_compose(rootReducer, {});
+
+export default store;
